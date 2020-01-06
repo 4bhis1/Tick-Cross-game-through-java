0d5183e0eaff58a7d19377d7c629a7e831ab6bfd
4bhis1 <57020242+4bhis1@users.noreply.github.com>
2020-01-06 15:52:48 +0530

    Add files via upload

---
+++ JOptionPane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package games;
+
+/**
+ *
+ * @author admin
+ */
+class JOptionPane {
+
+    static String showInputDialog(Object object, String enter_the_string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ player.java
@@ -0,0 +1,338 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package games;
+
+/**
+ *
+ * @author admin
+ */
+public class player extends javax.swing.JFrame {
+
+    /**
+     * Creates new form player
+     */
+    public player() {
+        initComponents();
+    
+    
+        
+import java.awt.event.MouseMotionListener;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+
+public class RedBall extends JComponent implements ActionListener, MouseMotionListener, KeyListener {
+
+    private int ballx = 150;
+    private int bally = 30;
+    private int ballx1 = 100;
+    private int bally1 = 10;
+    private int paddlex = 0;
+    private int ballySpeed = 7;
+    private int ballxSpeed = 5;
+    private int bally1Speed = 14;
+    private int ballx1Speed = 10;
+    public int score = 0;
+    public int score1 = 0;
+    private int scorefinal;
+    public int bestscore;
+    public int bestscore1;
+    public boolean gameOver, started;
+
+    public static void main(String[] args) {
+
+        JFrame wind = new JFrame("RedBall/GamePinfo");
+        RedBall g = new RedBall();
+        wind.add(g);
+        wind.pack();
+        wind.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        wind.setLocationRelativeTo(null);
+        wind.setVisible(true);
+        wind.addMouseMotionListener(g);
+
+        Timer tt = new Timer(17, g);
+        tt.start();
+
+    }
+
+    public void newball(int ballx, int bally, int ballxspeed, int ballyspeed) {
+
+        ballx = 150;
+        bally = 30;
+        ballxspeed = 5;
+        ballyspeed = 7;
+
+        JOptionPane.showMessageDialog(null, "new ball !");
+
+        return;
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+
+        return new Dimension(800, 600);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        //draw the sky
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, 800, 600);
+
+        g.setColor(Color.GREEN);
+        g.fillRect(0, 550, 800, 100);
+
+        //draw the paddel
+        g.setColor(Color.black);
+        g.fillRect(paddlex, 500, 100, 20);
+
+        //draw the ball
+        g.setColor(Color.RED);
+        g.fillOval(ballx, bally, 30, 30);
+
+        //draw the ball_1
+        if (score >= 5) {
+            g.setColor(Color.BLACK);
+            g.fillOval(ballx1, bally1, 30, 30);
+
+        }
+        //score	
+        if (score >= 5) {
+            g.setColor(Color.red);
+            g.setFont(new Font("Arial", 8, 50));
+            g.drawString(String.valueOf(score + score1), 30 / 1 - 15, 80);
+        } else {
+            g.setColor(Color.white);
+            g.setFont(new Font("Arial", 8, 50));
+            g.drawString(String.valueOf(score), 30 / 1 - 15, 80);
+        }
+        // start && gameOver
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", 8, 50));
+
+        if (gameOver) {
+
+            g.drawString(String.valueOf(" Best Score :" + scorefinal), 50 / 1 - 15, 200);
+
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        ballx = ballx + ballxSpeed;
+        bally = bally + ballySpeed;
+
+        // Window Down 
+        if (ballx >= paddlex && ballx <= paddlex + 100 && bally >= 475) {
+
+            ballySpeed = -7;
+            score++;
+
+        }
+
+        if (bally >= 700 ) {
+
+            score = 0;
+            bally = 30;
+            gameOver = true;
+
+        }
+
+        // Window up
+        if (bally <= 0) {
+
+            ballySpeed = 7;
+
+        }
+
+        // Window right
+        if (ballx >= 775) {
+
+            ballxSpeed = -5;
+
+        }
+
+        // Window left
+        if (ballx <= 0) {
+
+            ballxSpeed = 5;
+
+        }
+
+        //**********************************************************************
+        ballx1 = ballx1 + ballx1Speed;
+        bally1 = bally1 + bally1Speed;
+
+        // Window down
+        if (ballx1 >= paddlex && ballx1 <= paddlex + 100 && bally1 >= 475) {
+
+            bally1Speed = -14;
+            score1++;
+
+        }
+
+        if (bally1 >= 700) {
+
+            score1 = 0;
+            bally1 = 10;
+
+
+        }
+
+        // Window up
+        if (bally1 <= 0) {
+
+            bally1Speed = 14;
+
+        }
+
+        // Window right
+        if (ballx1 >= 775) {
+
+            ballx1Speed = -10;
+
+        }
+
+        // Window left
+        if (ballx1 <= 0) {
+
+            ballx1Speed = 10;
+
+        }
+
+        //**********************************************************************
+        bestscore = score;
+        bestscore1 = score1;
+
+        if (scorefinal > bestscore) {
+
+            scorefinal = scorefinal;
+
+        } else {
+
+            scorefinal = bestscore1;
+            scorefinal = score + score1;
+        }
+        
+        if (scorefinal > bestscore) {
+
+            scorefinal = scorefinal;
+
+        } else {
+
+            scorefinal = bestscore;
+            scorefinal = score + score1;
+        }
+
+        
+        repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+        paddlex = e.getX() - 50;
+        repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+}
+
+
+
+        
+    
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(player.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(player.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(player.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(player.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new player().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
